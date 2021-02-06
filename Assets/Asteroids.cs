3c9ca9e9e9d1c0e8e1139726ad43e3a56104d42f
JensenBeard <jensenbeard00@gmail.com>
2021-02-06 15:12:51 +0000

    Added asteroid bounds and collision

---
+++ Assets/Asteroids.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroids : MonoBehaviour
+{
+    public float maxThrust = 10f;
+    public float maxTorque = 3f;
+    public Rigidbody2D rb;
+    private Camera mainCam;
+
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 thrust = new Vector2(Random.Range(-maxThrust, maxThrust), Random.Range(-maxThrust, maxThrust));
+        float torque = Random.Range(-maxTorque, maxTorque);
+
+        rb.AddForce(thrust);
+        rb.AddTorque(torque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkPosition();
+    }
+
+    private void checkPosition() 
+    {
+        Vector2 newPos = transform.position;
+
+        if (transform.position.x > screenRight)
+        {
+            newPos.x = screenLeft;
+        }
+        if (transform.position.x < screenLeft)
+        {
+            newPos.x = screenRight;
+        }
+        if (transform.position.y > screenTop)
+        {
+            newPos.y = screenBottom;
+        }
+        if (transform.position.y < screenBottom)
+        {
+            newPos.y = screenTop;
+        }
+
+        transform.position = newPos;
+    }
+}
