b6d26f210022cbef086b05afd266b918ee089018
JensenBeard <jensenbeard00@gmail.com>
2021-03-10 17:36:03 +0000

    Tutorial will now progress to the final stage, clearing asteroids.

--- Assets/Scripts/Level Scripts/TutorialController.cs
+++ Assets/Scripts/Level Scripts/TutorialController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TutorialController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject gameController;
     public GameObject Observer;
-    //public GameObject[] triggers;
+    
 
     public static int triggerCounter = 0;
 
     [SerializeField] private GameObject[] triggers;
+    public GameObject ObjectivePrompt;
+    public GameObject NextLevelPrompt;
 
+    private int maxScore = 5 * 3 + 2 * 10 * 3 + 4 * 15 * 3;
+    private float waitTime = 2f;
     private int progress;
     private void Start()
     {
@@ -45,18 +50,27 @@ public class TutorialController : MonoBehaviour
         }
         else if (progress == 2)
         {
-            Debug.Log("Progress");
-             //Asteroids (Reach score)
-                /*
-                if (curScore >= maxScore)
-                {
-                    bool status = true;
-                    gameController.GetComponent<GameController>().setObjectiveComplete(status);
-                    ObjectivePrompt.SetActive(false);
-                    NextLevelPrompt.SetActive(true);
-                }*/
+            
+            //Asteroids (Reach score)
+            if (waitTime <= 0)
+            {
+                gameController.SetActive(true);
+                //ObjectivePrompt.GetComponent<TextMeshPro>().SetText("Press SPACE to fire photons to destroy all Asteroids!");
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+            
         }
 
-
+        int curScore = gameController.GetComponent<GameController>().getScore();
+        if (curScore >= maxScore)
+        {
+            bool status = true;
+            gameController.GetComponent<GameController>().setObjectiveComplete(status);
+            //ObjectivePrompt.GetComponent<TextMeshPro>().SetText("");
+            NextLevelPrompt.SetActive(true);
+        }
     }
 }
