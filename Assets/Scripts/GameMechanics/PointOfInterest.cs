a4f2f57bb0a69c1c89e43cd04611c50cd0508f03
JensenBeard <jensenbeard00@gmail.com>
2021-03-10 13:36:17 +0000

    Working Observer Pattern On Level 3

---
+++ Assets/Scripts/GameMechanics/PointOfInterest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : Subject
+{
+    [SerializeField] private string poiName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        Notify(poiName, NotificationType.AchievementUnlocked);
+    }
+}
--- Assets/Scripts/GameMechanics/TutorialSystem.cs
+++ Assets/Scripts/GameMechanics/TutorialSystem.cs
@@ -2,7 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialSystem : MonoBehaviour
+public class TutorialSystem : Observer
 {
-    
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        foreach (var poi in FindObjectsOfType<PointOfInterest>())
+        {
+            poi.RegisterObserver(this);
+        }
+    }
+
+    public override void OnNotify(object value, NotificationType notificationType) 
+    {
+        if (notificationType == NotificationType.AchievementUnlocked) 
+        {
+            string achievementKey = "achievement-" + value;
+
+           if (PlayerPrefs.GetInt(achievementKey) == 1) 
+               return;
+
+            PlayerPrefs.SetInt(achievementKey, 1);
+            Debug.Log("Unlocked " + value);
+        }
+    }
+}
+
+public enum NotificationType
+{
+    AchievementUnlocked
 }
