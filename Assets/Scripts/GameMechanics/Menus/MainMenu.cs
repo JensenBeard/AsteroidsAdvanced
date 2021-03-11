dfcf4c5d665f67590d0316e135f8a52b441457c1
JensenBeard <jensenbeard00@gmail.com>
2021-03-11 15:13:02 +0000

    Tutorial almost finalised and reorganised scenes.

--- Assets/Scripts/GameMechanics/Menus/MainMenu.cs
+++ Assets/Scripts/GameMechanics/Menus/MainMenu.cs
@@ -6,14 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public int playerHealth;
-    public int playerScore;
-
     public void playGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        PlayerPrefs.SetInt("PlayerHealth", playerHealth);
-        PlayerPrefs.SetInt("PlayerScore", playerScore);
     }
 
     public void QuitGame()
--- Assets/Scripts/Level Scripts/TutorialController.cs
+++ Assets/Scripts/Level Scripts/TutorialController.cs
@@ -1,24 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject gameController;
     public GameObject Observer;
-    
+
+    public int playerHealth;
+    public int playerScore;
+    float waitTime = 2f;
 
     public static int triggerCounter = 0;
 
     [SerializeField] private GameObject[] triggers;
-    public GameObject ObjectivePrompt;
     public GameObject NextLevelPrompt;
 
     private int maxScore = 5 * 3 + 2 * 10 * 3 + 4 * 15 * 3;
-    private float waitTime = 2f;
     private int progress;
+
+    [SerializeField] private Text ObjectivePrompt;
+    [SerializeField] private GameObject introObject;
+
     private void Start()
     {
         gameController.GetComponent<GameController>().setAsteroidNumber(3);
@@ -32,12 +37,9 @@ public class TutorialController : MonoBehaviour
 
         if (progress == 0)
         {//intro
+            Invoke("activateTriggers", 5);
+            
             progress++;
-            for (int i = 0; i < triggers.Length; i++) 
-            {
-                triggers[i].SetActive(true);
-            }
-            Observer.SetActive(true);
         }
         else if (progress == 1)
         {//Collect 3 objects
@@ -50,12 +52,14 @@ public class TutorialController : MonoBehaviour
         }
         else if (progress == 2)
         {
-            
+
             //Asteroids (Reach score)
             if (waitTime <= 0)
             {
+                PlayerPrefs.SetInt("PlayerHealth", playerHealth);
+                PlayerPrefs.SetInt("PlayerScore", playerScore);
                 gameController.SetActive(true);
-                //ObjectivePrompt.GetComponent<TextMeshPro>().SetText("Press SPACE to fire photons to destroy all Asteroids!");
+                ObjectivePrompt.text = "Objective: Press SPACE to fire photons to destroy all Asteroids!";
             }
             else
             {
@@ -69,8 +73,20 @@ public class TutorialController : MonoBehaviour
         {
             bool status = true;
             gameController.GetComponent<GameController>().setObjectiveComplete(status);
-            //ObjectivePrompt.GetComponent<TextMeshPro>().SetText("");
+            ObjectivePrompt.text = "Objective: Follow the Arrow to your next destination";
             NextLevelPrompt.SetActive(true);
         }
+    }
+
+
+    private void activateTriggers()
+    {
+        introObject.SetActive(false);
+        ObjectivePrompt.gameObject.SetActive(true);
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            triggers[i].SetActive(true);
+        }
+        Observer.SetActive(true);
     }
 }
