13f8beeccf100c08a65e8e94025a59655eef48ac
JensenBeard <jensenbeard00@gmail.com>
2021-03-16 22:56:25 +0000

    Enemy ship projectiles are now lethal and ships will spawn in random locations.

--- Assets/Scripts/GameMechanics/PlayerScripts/fireball.cs
+++ Assets/Scripts/GameMechanics/PlayerScripts/fireball.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class fireball : MonoBehaviour
 {
     private float lifespan = 3f;
+    public GameObject controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+
         GetComponent<Rigidbody2D>().AddForce(transform.up * 350);
         KillOldFireball(lifespan);
-        
+        controller = GameObject.Find("GameController");
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject, 0.0f);
+        
     }
 
-    void KillOldFireball(float lifeSpan) 
+    void KillOldFireball(float lifeSpan)
     {
         Destroy(gameObject, lifeSpan);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            int damage = 20;
+            controller.SendMessage("playerDamage", damage);
+        }
+    }
 }
    
--- Assets/Scripts/Level Scripts/Level4Script.cs
+++ Assets/Scripts/Level Scripts/Level4Script.cs
@@ -7,18 +7,32 @@ public class Level4Script : MonoBehaviour
     public GameObject gameController;
     public GameObject ObjectivePrompt;
     public GameObject[] NextLevelPrompt;
+    public GameObject EnemyShip;
     private int maxScore;
+
+    [SerializeField] private float leftMax = 21f;
+    [SerializeField] private float leftMin = 19F;
+    [SerializeField] private float topMax = 12f;
+    [SerializeField] private float topMin = 11f;
+    [SerializeField] private float rightMax = -21f;
+    [SerializeField] private float rightMin = -19F;
+    [SerializeField] private float botMax = -12f;
+    [SerializeField] private float botMin = -11f;
+    [SerializeField] private int numEnemys;
+
     private void Start()
     {
         gameController.GetComponent<GameController>().setAsteroidNumber(5);
         maxScore = PlayerPrefs.GetInt("PlayerScore") + (5 * 5 + 2 * 10 * 3 + 4 * 15 * 5);
         gameController.SetActive(true);
         ObjectivePrompt.SetActive(true);
+        SpawnEnemyShips();
     }
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         int curScore = gameController.GetComponent<GameController>().getScore();
+        
         if (curScore >= maxScore)
         {
             bool status = true;
@@ -26,5 +40,64 @@ public class Level4Script : MonoBehaviour
             ObjectivePrompt.SetActive(false);
         }
         //Can maybe spawn some enemy ai at some point
+    }
+
+    void SpawnEnemyShips()
+    {
+        for (int i = 0; i < numEnemys; i++)
+        {
+            float[] location = SafeSpawnShip();
+            Instantiate(EnemyShip, new Vector3(location[0], location[1], 0), Quaternion.Euler(0, 0, Random.Range(-0.0f, 359.0f)));
+        }
+    }
+
+    float[] SafeSpawnShip()
+    {
+
+        float height;
+        float width;
+
+        int key = Random.Range(0, 1);
+        //Spawning top/bottom(1) or right/left(0)
+        if (key == 1)
+        {
+            //determine if spawning on top or bottom
+            // 1 = top
+            key = Random.Range(0, 1);
+            if (key == 1)
+            {
+                width = Random.Range(-18.0f, 18.0f);
+                height = Random.Range(topMin, topMax);
+
+            }
+            else
+            {
+                width = Random.Range(-18.0f, 18.0f);
+                height = Random.Range(botMin, botMax);
+
+            }
+        }
+        else
+        {
+            key = Random.Range(0, 1);
+            if (key == 1)
+            {
+                height = Random.Range(-12.0f, 12.0f);
+                width = Random.Range(leftMin, leftMax);
+
+            }
+            else
+            {
+                height = Random.Range(-12.0f, 12.0f);
+                width = Random.Range(rightMin, rightMax);
+
+            }
+        }
+
+
+
+
+        float[] location = new float[] { width, height };
+        return location;
     }
 }
