b717fe374833e17280ed9b78957ba3dbc417282b
JensenBeard <jensenbeard00@gmail.com>
2021-05-03 13:10:10 +0100

    Revert "Added UI sound"
    This reverts commit f7bc94927230c111c60cbab78c5cd3c58ea8c8e7.

--- Assets/Scripts/GameMechanics/Menus/MainMenu.cs
+++ Assets/Scripts/GameMechanics/Menus/MainMenu.cs
@@ -1,27 +1,19 @@
-﻿using ChrisTutorials.Persistent;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private AudioClip _audioClip;
-
-    public void playGame()
+    public void playGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+        
     }
 
     public void QuitGame()
     {
         Debug.Log("Game Quit");
         Application.Quit();
-    }
-
-    public void playButtonClick()
-    {
-        AudioManager.Instance.Play(_audioClip, transform);
     }
 }
